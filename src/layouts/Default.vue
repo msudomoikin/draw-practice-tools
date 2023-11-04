672046b2a6c386781a3eda6050d8b35f6d2b6464
Matvey Sudomoykin <me@matveymatvey.com>
2023-11-04 04:07:05 +0300

    default layout

---
+++ src/layouts/Default.vue
@@ -0,0 +1,30 @@
+<template>
+    <div class="flex h-screen w-screen">
+      <Sidebar></Sidebar>
+      <!-- Content -->
+      <div class="flex-1 flex flex-col">
+        <!-- Topbar -->
+        <Topbar></Topbar>
+        <!-- Main content -->
+        <div class="flex-1 p-4 overflow-auto">
+          <slot></slot>
+        </div>
+      </div>
+    </div>
+  </template>
+  
+  <script>
+import Sidebar from '../components/Sidebar.vue';
+import Topbar from '../components/Topbar.vue';
+
+
+  export default {
+    name: "DefaultLayout",
+    components: { Sidebar, Topbar }
+}
+  </script>
+  
+  <style scoped>
+  /* Здесь может быть ваш CSS */
+  </style>
+  
